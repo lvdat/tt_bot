5e0535af9e389f82b70952a600ed17b0a249757d
lvdat <lvdat13@gmail.com>
2026-04-21 09:38:17 +0700

    feat: set level up channel command

---
+++ commands/set-level-channel.js
@@ -0,0 +1,18 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js'
+import { setLevelChannel } from '../modules/level/config/guild.service.js'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('set-level-channel')
+		.setDescription('Set kênh thông báo level')
+		.addChannelOption((o) => o.setName('channel').setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+	async execute(interaction) {
+		const ch = interaction.options.getChannel('channel')
+
+		await setLevelChannel(interaction.guild.id, ch.id)
+
+		await interaction.reply(`✅ Đã set kênh ${ch}`)
+	},
+}
