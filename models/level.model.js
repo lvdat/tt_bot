d8310fa05b097da47e8523d4f3b3955cc6f6cacd
lvdat <lvdat13@gmail.com>
2026-04-21 09:01:35 +0700

    feat: level model

---
+++ models/level.model.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+  guildId: String,
+  userId: String,
+  xp: { type: Number, default: 0 },
+  level: { type: Number, default: 1 }
+})
+
+schema.index({ guildId: 1, userId: 1 }, { unique: true })
+
+export default mongoose.model('Level', schema)
