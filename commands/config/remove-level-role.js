10e39e614e3399ea8c703948806d2e5bb3a02f5c
lvdat <lvdat13@gmail.com>
2026-04-21 09:40:12 +0700

    feat: remove level role command

---
+++ commands/config/remove-level-role.js
@@ -0,0 +1,18 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js'
+import { removeLevelRole } from '../../modules/level/config/guild.service.js'
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('remove-level-role')
+		.setDescription('Xóa role theo level')
+		.addIntegerOption((o) => o.setName('level').setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+	async execute(interaction) {
+		const level = interaction.options.getInteger('level')
+
+		await removeLevelRole(interaction.guild.id, level)
+
+		await interaction.reply(`🗑️ Đã xóa role level ${level}`)
+	},
+}
